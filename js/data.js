d2e0b6e8c2b5ed31feb55fabbf4011295cbafe43
promtfaiz <fz.private19@yahoo.com>
2025-05-25 00:19:09 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,36 @@
+ {
+        "@context": "https://schema.org",
+        "@type": "ProfessionalService",
+        "name": "BananeWala",
+        "image": "https://bananewala.in.net/images/BananeWala-Logo.png",
+        "@id": "https://bananewala.in.net",
+        "url": "https://bananewala.in.net",
+        "telephone": "+919145592101",
+        "priceRange": "₹5000 - ₹15000",
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Pune",
+            "addressRegion": "Maharashtra",
+            "postalCode": "411001",
+            "addressCountry": "IN"
+        },
+        "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": 18.5204,
+            "longitude": 73.8567
+        },
+        "openingHoursSpecification": {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            ],
+            "opens": "10:00",
+            "closes": "19:00"
+        }
+    }
